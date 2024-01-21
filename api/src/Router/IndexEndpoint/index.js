13512f55726c150cc6cbc15e4655e0199dd42463
Carlex Mandou <carlexmandou@gmail.com>
2024-01-21 22:07:33 +0100

    Resolving IndexController path

--- api/src/Router/IndexEndpoint/index.js
+++ api/src/Router/IndexEndpoint/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const IndexController = require("../../Controllers/indexController");
+const IndexController = require("../../Controllers/IndexController");
 const TTSController = require("../../Controllers/TTSController");
 
 router.get("/", IndexController.SendAPIDescription);
