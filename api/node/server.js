4e2ca4d6cdf9bf09df13df85b698bcdbe1bc1eb7
Carlex Mandou <carlexmandou@gmail.com>
2024-01-21 01:01:49 +0100

    _

--- api/node/server.js
+++ api/node/server.js
@@ -12,59 +12,63 @@ app.use(cors());
 app.use(express.urlencoded({ extends: true }));
 
 const client = new openAI({ apiKey });
-async function TextTospeech(param, cb) {
-    client.audio.speech.create({
-        model: 'tts-1',
-        voice: param.voice,
-        speed: param.speed,
-        input: param.input
-    }).then(response => {
-        cb(response);
-    }).catch(error => {
-        cb(undefined, error)
-    });
-}
+
 app.get("/", (req, res) => {
     res.status(200).send("api is working fine");
 });
 
 app.post("/to-speech", async (req, res) => {
     const param = {
-        voice: req.body.voice || 'alloy',
-        speed: req.body.speed || 1,
-        input: req.body.text || 'Aujourd\'hui est une journée merveilleuse pour construire quelque chose que les gens aiment!'
+        voice: req.body.voice,
+        speed: req.body.speed,
+        input: req.body.text
     }
     console.log(param);
-    await TextTospeech(param, (response, error) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({ msg: "Something went wrong", error });
-        } else {
-            const audioBuffer = Buffer.from(response.data, "base64");
-            res.setHeader('Content-Type', 'audio/mpeg');
-            res.setHeader('Content-Disposition', 'attachment; filename=Ausio.mp3');
-            res.status(200).send(audioBuffer);
-        }
-    })
+    await TextTospeech(param, SendResponse)
 })
 
 app.get("/test/to-speech", async (req, res) => {
     const param = {
-        voice: req.query.voice || 'alloy',
-        speed: req.query.speed || 1,
-        input: req.query.text || 'Aujourd\'hui est une journée merveilleuse pour construire quelque chose que les gens aiment!'
+        voice: req.query.voice || "alloy",
+        speed: req.query.speed || "1",
+        input: req.query.text || "Aujourd\'hui est une journée merveilleuse pour construire quelque chose que les gens aiment!"
     }
     console.log(param);
-
-    await TextTospeech(param, (response, error) => {
-        if (error) {
-            res.status(500).send(error);
-        } else {
-            console.log(response);
-            console.log(response.data);
-            res.status(200).send(response.data)
-        }
-    })
+    await TextTospeech(param, SendResponse)
 })
+
+
+function SendResponse(response, error) {
+    if (error) {
+        console.log(error);
+        res.status(500).send(error);
+    } else {
+        console.log(response);
+        res.status(200).send(response.data)
+
+        // if (error) {
+        //     console.log(error);
+        //     res.status(500).json({ msg: "Something went wrong", error });
+        // } else {
+        //     const audioBuffer = Buffer.from(response.data, "base64");
+        //     res.setHeader('Content-Type', 'audio/mpeg');
+        //     res.setHeader('Content-Disposition', 'attachment; filename=Ausio.mp3');
+        //     res.status(200).send(audioBuffer);
+        // }
+    }
+}
+
+async function TextTospeech(param, cb) {
+    client.audio.speech.create({
+        model: "tts-1",
+        voice: param.voice.toString(),
+        speed: param.speed,
+        input: param.input.toString()
+    }).then(response => {
+        cb(response);
+    }).catch(error => {
+        cb(undefined, error)
+    });
+}
 
 app.listen(PORT);
